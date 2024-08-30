cf212e58bced4ad029d82efddc9f06fc449d30ed
BenyminRmb <benyaminrmb@gmail.com>
2024-08-30 12:26:24 +0330

    (Feat): created circle.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="drawer flex flex-wrap w-full">
     <input id="my-drawer" class="drawer-toggle peer" type="checkbox" />
-    <div class="drawer-content w-full peer-checked:-translate-x-14 transition ease-out">
-      <div class="flex flex-wrap w-full gap-6">
-        <div class="flex flex-wrap w-full container mx-auto gap-6 px-3">
+    <div
+      class="drawer-content w-full peer-checked:-translate-x-14 transition ease-out">
+      <div class="flex flex-wrap w-full gap-6 z-0 relative">
+        <img
+          alt="bubble"
+          class="absolute right-0 top-32 -z-10 w-60"
+          src="/circle.svg" />
+
+        <div
+          class="flex flex-wrap w-full container relative mx-auto gap-6 px-3">
           <main-header-navbar />
           <main-header-breadcrumb />
         </div>
-        <div class="flex h-full flex-wrap w-full grow container mx-auto">
+        <div
+          class="flex h-full relative flex-wrap w-full grow container mx-auto">
+          <span>ddd </span>
           <slot />
         </div>
         <main-footer-main />
