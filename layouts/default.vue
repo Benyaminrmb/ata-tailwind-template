f4ec8f76f1958dc9bd3820f90b000ebf9e30041d
Benyamin Rmb <benyaminrmb@gmail.com>
2024-09-01 22:35:29 +0330

    (Fix): changed the color of drawer and moved header out of it.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,34 @@
 <template>
-  <div class="drawer flex flex-wrap w-full">
-    <input id="my-drawer" class="drawer-toggle peer" type="checkbox" />
-    <div
-      class="drawer-content w-full peer-checked:-translate-x-14 transition ease-out">
-      <div class="flex flex-wrap w-full gap-6 z-0 relative">
-        <img
-          alt="bubble"
-          class="absolute right-0 top-32 -z-10 w-60"
-          src="/circle.svg" />
+  <div
+    class="flex flex-wrap w-full overflow-hidden container relative mx-auto px-3">
+    <main-header-navbar />
+    <div class="drawer !h-full flex flex-wrap w-full">
+      <input id="my-drawer" class="drawer-toggle peer" type="checkbox" />
 
-        <div
-          class="flex flex-wrap w-full container relative mx-auto gap-6 px-3">
-          <main-header-navbar />
-          <main-header-breadcrumb :is-breadcrumb="false"/>
-        </div>
-        <div
-          class="flex h-full relative flex-wrap w-full grow container mx-auto px-3">
+      <div
+        class="drawer-content !h-full w-full peer-checked:-translate-x-14 transition ease-out">
+        <div class="flex mt-6 flex-wrap w-full gap-6 z-0 relative">
+          <img
+            alt="bubble"
+            class="absolute right-0 top-32 -z-10 w-60"
+            src="/circle.svg" />
 
-          <slot />
+          <main-header-breadcrumb :is-breadcrumb="false" />
+
+          <div class="flex h-full relative flex-wrap w-full grow">
+            <slot />
+          </div>
+          <main-footer-main />
         </div>
-        <main-footer-main />
       </div>
-    </div>
-    <div class="drawer-side">
-      <label
-        aria-label="close sidebar"
-        class="drawer-overlay"
-        for="my-drawer"></label>
-      <div class="bg-base-200 text-base-content min-h-full w-80 p-4">
-        <main-header-menu-ul class="has-border menu-lg" />
+      <div class="drawer-side !h-full top-auto">
+        <label
+          aria-label="close sidebar"
+          class="drawer-overlay"
+          for="my-drawer"></label>
+        <div class="bg-secondary text-secondary-content min-h-full w-80 p-4">
+          <main-header-menu-ul class="has-border menu-lg" />
+        </div>
       </div>
     </div>
   </div>
